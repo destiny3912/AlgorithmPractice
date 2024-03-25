8df294292fac71ac17d137d83b84750f6a2c0344
이지호 <lho@pntbiz.com>
2024-03-25 13:01:14 +0900

    [BOJ] 11660

---
+++ src/BOJ_11660/Main.java
@@ -0,0 +1,33 @@
+package BOJ_11660;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static Integer N, M;
+    private int[][] map;
+    private int
+    public static void main(String[] args) throws IOException {
+        read();
+
+
+    }
+
+    private static void read() throws IOException {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String[] tokens = br.readLine().split(" ");
+
+            H = Integer.parseInt(tokens[0]);
+            W = Integer.parseInt(tokens[1]);
+
+            for(int i = 0; i < N; i++) {
+                tokens = br.readLine().split(" ");
+
+                for (String token : tokens) {
+
+                }
+            }
+        }
+    }
+}
